0fa1dab83cd459da0315d91d2542c3879147f0e7
Im Genius <im.genius.man@gmail.com>
2017-07-10 15:10:00 +0500

    Client Side Searching and Related Products fixed

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -3,16 +3,19 @@ Spree::ProductsHelper.class_eval do
         product.price
     end
 
-    def related_products(product, max = 4)
+    def related_products(orignal, max = 8)
         products = [];
-        unless product.taxons.blank?
-           product.taxons.each do |taxons|
-            taxon_preview(taxons).each do |product|
-                products << product unless products.size > max
-                break if products.size >= max
+        unless orignal.taxons.blank?
+            orignal.taxons.each do |taxons|
+                taxon_preview(taxons).each do |product|
+                    products << product unless products.size > max || orignal == product
+                    break if products.size >= max
+                end
             end
-           end
         end
-        products
+        products.uniq
+    end
+    def first_level_taxons(taxons)
+        taxons.where(parent_id: 1).or(taxons.where(parent_id: 5))
     end
 end
